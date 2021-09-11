c5fbcfe4432734e32f8da52c31d9c96c0dab27c0
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-09-11 23:48:22 +0300

    add count_on_page helper

--- app/Ship/Helpers/helpers.php
+++ app/Ship/Helpers/helpers.php
@@ -10,3 +10,34 @@
 | All files under app/{section_name}/{container_name}/Helpers/ folder will be autoloaded by Laraneat.
 |
 */
+
+if (!function_exists('count_on_page')) {
+    /**
+     * @param int $page
+     * @param int $total
+     * @param int|null $perPage
+     *
+     * @return int
+     * @throws Exception
+     */
+    function count_on_page(int $page, int $total, ?int $perPage = null): int
+    {
+        $perPage = $perPage ?? (int) config('json-api-paginate.default_size', 0);
+
+        if (!($perPage > 0)) {
+            throw new \Exception("perPage argument not passed");
+        }
+
+        $lastPage = (int) ceil($total / $perPage);
+
+        if ($page === $lastPage) {
+            return $total - ($page - 1) * $perPage;
+        }
+
+        if ($page > $lastPage) {
+            return 0;
+        }
+
+        return $perPage;
+    }
+}
