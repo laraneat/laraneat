9248176ac20103f5f3bdff8d4e47d8a0453b33d7
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-09-15 00:21:05 +0300

    add EloquentModelWizard

---
+++ app/Ship/Abstracts/QueryWizards/EloquentModelWizard.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Ship\Abstracts\QueryWizards;
+
+use Jackardios\QueryWizard\EloquentModelWizard as BaseEloquentModelWizard;
+
+abstract class EloquentModelWizard extends BaseEloquentModelWizard
+{
+    //
+}
