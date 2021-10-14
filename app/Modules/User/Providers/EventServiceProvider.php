f808bcd3c714079bccc02c1a976bbf317ba6e7a1
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-10-14 16:22:37 +0300

    refactor

---
+++ app/Modules/User/Providers/EventServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Modules\User\Providers;
+
+use App\Ship\Abstracts\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        Registered::class => [
+            SendEmailVerificationNotification::class,
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
